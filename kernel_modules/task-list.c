49cd2bac95f618b0a36830777dbc79d14a79e626
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2020-10-08 15:41:36 +0700

    Refactor code

--- kernel_modules/task-list.c
+++ kernel_modules/task-list.c
@@ -1,7 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched/signal.h>
-#include<linux/types.h>
 
 char buffer[256];
 char * get_task_state(long state)
@@ -27,39 +26,14 @@ char * get_task_state(long state)
 
 static int test_tasks_init(void)
 {
-    struct task_struct *task_list, *test_list;
-    struct mm_struct *m_area;
-    struct thread_info info;
-    struct list_head *p;
+    struct task_struct *task_list;
     unsigned int process_count = 0;
     pr_info("%s: In init\n", __func__);
     for_each_process(task_list) {
-        pr_info("Process: %s\t PID:[%d]\t Processor:[%d]\t State:%s\n",
+        pr_info("Process: %s\t PID:[%d]\t State:%s\n", 
                 task_list->comm, task_list->pid,
-		task_list->cpu,
                 get_task_state(task_list->state));
-/*	list_for_each(p, &task_list->tasks) {
-		test_list = list_entry(p, struct task_struct, tasks);
-		pr_info ("Process: %s\t", test_list->comm);
-	}
- 
-	info = task_list->thread_info;
-	pr_info ("Cpu number: %d\t", info.flags);
-*/
-
-	m_area = task_list->mm;
-
-	if (m_area != NULL) {
-		pr_info("PGD of process: %lu", m_area->pgd);
-	}
-
-	if (m_area->owner = task_list) {
-		pr_info("Giong nhau") ;
-	}
-
-	pr_info("Rlimit: %lu", task_list->signal->rlim[RLIMIT_DATA].rlim_cur); 
-
-	process_count++;
+        process_count++;    
     }
     pr_info("Number of processes:%u\n", process_count);
     return 0;
